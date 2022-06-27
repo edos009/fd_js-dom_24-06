3bf7e249175062d5cd85ed6a42e1d050f44f8f00
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-27 22:18:39 +0300

    Example Counter - closure v2.0

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -2,18 +2,21 @@
 
 function createCounter(initial) {
   let i = initial; //closure
-  return function counter() {
-    i++;
-    return i;
+  return {
+    increment() {
+      return ++i;
+    },
+    decrement() {
+      return --i;
+    },
   };
 }
 
 const counter1 = createCounter(30);
-counter1();
-counter1();
-counter1();
-console.log(counter1());
+counter1.increment();
+counter1.increment();
+counter1.increment;
+console.log(counter1.increment());
 
 const counter2 = createCounter(100);
-console.log(counter2());
-
+console.log(counter2.decrement());
