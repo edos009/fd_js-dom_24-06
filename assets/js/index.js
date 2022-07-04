c8d4b716257c89da6a63b219d29db939b487577b
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-04 15:58:12 +0300

    Example with setTimeout and setInterval

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,79 +1,22 @@
-"use strict";
+'use strict'
 
-const messages = [];
+// const btn = document.getElementById('btn');
 
-const form = document.getElementById("form");
-const sms = document.querySelector(".sms");
-let collectionSms;
+// btn.addEventListener('click', btnHandler);
 
-form.addEventListener("submit", checkArray);
+// function btnHandler(){
 
-function checkArray(e) {
-  e.preventDefault();
-  const reg = /^[A-Z][a-z]{2,12} [A-Z]\.$/;
-  let textValue = e.target.text.value.trim();
+// }
 
-  if (messages.includes(textValue) || textValue === "") {
-    e.target.reset();
-    return;
-  } else {
-    if (reg.test(textValue)) {
-      messages.push(textValue);
 
-      const pElem = createElement(
-        "p",
-        { events: { click: setBackgroundColorElement } },
-        document.createTextNode(textValue)
-      );
+// console.log(1);
+// setTimeout(() => {
+//   console.log(3);
+// }, 1500);
+// console.log(2);
 
-      const buttonElem = createElement(
-        "button",
-        {
-          events: { click: handleDeleteSms.bind(pElem) },
-          attributes: { "data-sms-value": textValue },
-        },
-        document.createTextNode("X")
-      );
+const identificator = setTimeout(() => {
+  console.log(3);
+}, 500);
 
-      pElem.append(buttonElem);
-
-      sms.append(pElem);
-    }
-    e.target.reset();
-  }
-}
-
-function createElement(
-  tag,
-  { className = [], attributes = {}, events = {} },
-  ...children
-) {
-  const elem = document.createElement(tag);
-  elem.append(...children);
-
-  for (const [typeEvent, handleEvent] of Object.entries(events)) {
-    elem.addEventListener(typeEvent, handleEvent);
-  }
-
-  for (const [nameAttr, valueAttr] of Object.entries(attributes)) {
-    elem.setAttribute(nameAttr, valueAttr);
-  }
-
-  return elem;
-}
-
-function setBackgroundColorElement(e) {
-  for (const elem of document.querySelectorAll(".add-color-text")) {
-    elem.classList.remove("add-color-text");
-  }
-
-  e.target.classList.add("add-color-text");
-}
-
-function handleDeleteSms({target}) {
-  const valueSms = target.dataset.smsValue;
-  this.remove();
-
-  messages.splice(messages.indexOf(valueSms), 1);
-  console.log(messages);
-}
+clearTimeout(identificator);
