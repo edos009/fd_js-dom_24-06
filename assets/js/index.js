f2496183d37572ee1ec91d2faa1c5fc1535e11d2
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-05 14:56:38 +0300

    Create file data.json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,40 @@
 "use strict";
 
-const user = {
-  fname: 'Elon',
-  age: 51,
-  getName(){
-    return this.fname
-  },
-  isWife: true,
-  children: ['ch1', 'ch2'],
-  adress: {
-    country: 'USA',
-    town: 'NY'
-  },
-  ukrPassport: null,
-  petty: undefined,
-  [Symbol('prop symbol')]: 'prop symbol'
+// const user = {
+//   fname: "Elon",
+//   age: 51,
+//   getName() {
+//     return this.fname;
+//   },
+//   isWife: true,
+//   children: ["ch1", "ch2"],
+//   address: {
+//     country: "USA",
+//     town: "NY",
+//   },
+//   ukrPassport: null,
+//   petty: undefined,
+//   [Symbol("prop symbol")]: "prop symbol",
+// };
+
+// const serializationUser = JSON.stringify(user);
+// console.log(serializationUser);
+
+// const deserializationUser = JSON.parse(serializationUser);
+// console.log(deserializationUser);
+
+const animal = {
+  type: "cat",
+  age: 10,
+  isMustache: true,
+  children: ["kitty1, kitty2"],
+  color: null,
+  countLives: undefined,
 };
 
-const serializationUser = JSON.stringify(user);
-console.log(serializationUser);
+console.log(animal);
+const serializationAnimal = JSON.stringify(animal);
+console.log(serializationAnimal);
 
-const deserializationUser = JSON.parse(serializationUser);
-console.log(deserializationUser);
+const deserializationAnimal = JSON.parse(serializationAnimal);
+console.log(deserializationAnimal);
