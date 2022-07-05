d6160cb66ad82113dba90bcd63ea85edb2845203
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-05 16:34:49 +0300

    Example with work promise and data

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,20 +16,15 @@
 // const deserializationAnimal = JSON.parse(serializationAnimal);
 // console.log(deserializationAnimal);
 
-//Example 1
-const promise1 = fetch('./assets/js/data.json');
-console.log(promise1);
-
-promise1.then((response) => {
-  console.log(response);
-  const dataJson = response.json();
-  console.log(dataJson);
-  dataJson.then((data) => {
-    console.log(data);
-  })
-})
-
 //Example 2
+// fetch("./assets/js/data.json")
+//   .then((response) => response.json())
+//   .then((data) => console.table(data))
+//   .catch((error) => console.log(error)) //для отловки ошибок
+//   .finally(()=> console.log('finally')); //не зависимо от результата вернет файнали
+
 fetch("./assets/js/data.json")
   .then((response) => response.json())
-  .then((data) => console.table(data));
+  .then((data) => {
+    console.log(data.map((item) => item.type).join(", "));
+  });
